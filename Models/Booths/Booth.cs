a2c431603c1685fc354fd08044357091f0b38b78
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-04-08 00:10:41 +0300

    Commit with created the basic classes.

---
+++ Models/Booths/Booth.cs
@@ -0,0 +1,42 @@
+﻿using ChristmasPastryShop.Models.Booths.Contracts;
+using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Models.Delicacies.Contracts;
+using ChristmasPastryShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Booths
+{
+    public class Booth : IBooth
+    {
+        public int BoothId => throw new NotImplementedException();
+
+        public int Capacity => throw new NotImplementedException();
+
+        public IRepository<IDelicacy> DelicacyMenu => throw new NotImplementedException();
+
+        public IRepository<ICocktail> CocktailMenu => throw new NotImplementedException();
+
+        public double CurrentBill => throw new NotImplementedException();
+
+        public double Turnover => throw new NotImplementedException();
+
+        public bool IsReserved => throw new NotImplementedException();
+
+        public void ChangeStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Charge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCurrentBill(double amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/Cocktails/Cocktail.cs
@@ -0,0 +1,55 @@
+﻿using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Cocktails
+{
+    public abstract class Cocktail : ICocktail
+    {
+        private string name;
+        private string size;
+        private double price;
+        private string[] allowedSizeValues = new string[] { "Small", "Middle", "Large" };
+
+        public string Name 
+        {
+            get => name;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.NameNullOrWhitespace);
+                }
+                name = value;
+            }
+        }
+
+        public string Size 
+        {
+            get => size;
+            private set => size = value;
+        }
+
+        public double Price 
+        {
+            get => price;
+            private set
+            {
+                if (Size == "Large")
+                {
+                    price = value;
+                }
+                else if (Size == "Middle")
+                {
+                    price = 2* (value / 3.0);
+                }
+                else
+                {
+                    price = value / 3.0;
+                }
+            }
+        }
+    }
+}
